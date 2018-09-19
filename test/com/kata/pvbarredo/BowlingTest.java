cea920f6f2c6d173dd9d3e90e9b9ccb95e8d0f5f
Peter Barredo <pvbarredo@gmail.com>
2018-09-19 23:17:10 +0800

    Finished third case and refactor test
    given_all_shot_is_3_then_score_should_be_60

--- test/com/kata/pvbarredo/BowlingTest.java
+++ test/com/kata/pvbarredo/BowlingTest.java
@@ -9,10 +9,12 @@ import java.util.List;
 public class BowlingTest {
 
     BowlingGame bowlingGame;
+    List<Frame> frames;
 
     @Before
     public void setup(){
         bowlingGame = new BowlingGame();
+        frames = bowlingGame.getFrames();
     }
 
     @Test
@@ -25,12 +27,19 @@ public class BowlingTest {
     @Test
     public void given_all_shot_is_gutter_balls_then_score_should_be_0() {
 
-        List<Frame> frames = bowlingGame.getFrames();
-
         for (int i = 0; i < 10 ; i++) {
             frames.add(new Frame(0, 0));
         }
 
         Assert.assertEquals(0, bowlingGame.getScore());
+    }
+
+    @Test
+    public void given_all_shot_is_3_then_score_should_be_60() {
+        for (int i = 0; i < 10 ; i++) {
+            frames.add(new Frame(3, 3));
+        }
+
+        Assert.assertEquals(60, bowlingGame.getScore());
     }
 }
