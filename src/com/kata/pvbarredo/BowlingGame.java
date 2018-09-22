8f3bf2491cf3aac229397892d286fea546e08e9b
Peter Barredo <pvbarredo@gmail.com>
2018-09-22 09:12:07 +0800

    Done 5th test case game- perfect game TODO : refactoring

--- src/com/kata/pvbarredo/BowlingGame.java
+++ src/com/kata/pvbarredo/BowlingGame.java
@@ -18,7 +18,18 @@ public class BowlingGame {
 
         for (int i = 0; i < 10; i++) {
             Frame frame = frames.get(i);
-            if(frame.getFirstRoll()  + frame.getSecondRoll() == 10){
+            if(frame.getFirstRoll() == 10){
+                Frame secondFrame = frames.get(i + 1);
+                if(secondFrame.getFirstRoll() == 10){
+                    Frame thirdFrame = frames.get(i + 2);
+                    score += 20 + thirdFrame.getFirstRoll();
+
+                }else{
+                    score += 10 + secondFrame.getFirstRoll() + secondFrame.getSecondRoll();
+                }
+
+
+            }else if(isSpare(frame)){
 
                 Frame secondFrame = frames.get(i + 1);
                 score += 10 + secondFrame.getFirstRoll();
@@ -30,9 +41,11 @@ public class BowlingGame {
 
         }
 
-
-
         return score;
+    }
+
+    private boolean isSpare(Frame frame) {
+        return frame.getFirstRoll()  + frame.getSecondRoll() == 10;
     }
 
 
--- test/com/kata/pvbarredo/BowlingGameTest.java
+++ test/com/kata/pvbarredo/BowlingGameTest.java
@@ -12,7 +12,7 @@ public class BowlingGameTest {
     List<Frame> frames;
 
     @Before
-    public void setup(){
+    public void setup() {
         bowlingGame = new BowlingGame();
         frames = bowlingGame.getFrames();
     }
@@ -20,7 +20,7 @@ public class BowlingGameTest {
     @Test
     public void given_all_shot_is_gutter_balls_then_score_should_be_0() {
 
-        for (int i = 0; i < 10 ; i++) {
+        for (int i = 0; i < 10; i++) {
             frames.add(new Frame(0, 0));
         }
 
@@ -29,7 +29,7 @@ public class BowlingGameTest {
 
     @Test
     public void given_all_shot_is_3_then_score_should_be_60() {
-        for (int i = 0; i < 10 ; i++) {
+        for (int i = 0; i < 10; i++) {
             frames.add(new Frame(3, 3));
         }
 
@@ -39,13 +39,23 @@ public class BowlingGameTest {
     @Test
     public void given_10_pairs_of_5_and_spare_and_the_last_is_5_then_the_score_is_150() {
         for (int i = 0; i < 10; i++) {
-            frames.add(new Frame(5,5));
+            frames.add(new Frame(5, 5));
 
         }
-        frames.add(new Frame(5,0));
+        frames.add(new Frame(5, 0));
 
         Assert.assertEquals(150, bowlingGame.getScore());
+    }
 
+    @Test
+    public void given_perfect_game_then_score_should_be_300() {
+        for (int i = 0; i < 10; i++) {
+            frames.add(new Frame(10, 0));
 
+        }
+        frames.add(new Frame(10, 0));
+        frames.add(new Frame(10, 0));
+
+        Assert.assertEquals(300, bowlingGame.getScore());
     }
 }
